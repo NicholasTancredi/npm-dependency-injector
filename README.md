44566e6b730af3146b6e87597490b969a3ebe348
Nicholas Tancredi <nicholastancredi@Nicholass-MacBook-Air.local>
2016-10-27 21:54:29 -0400

    Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # npm-dependency-injector
-Dependecy injector for pure functional npm modules.
+## Dependecy injector for pure functional npm modules.
+Require your package.json file ```javascript require('./package.json')``` as the first
+argument and your modules root function as the second argument followed
+by any additional arguments.
--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-module.exports = (
-    {dependencies},
-    func,
-    ...args
-) => func.bind(
-    null,
+module.exports = ({dependencies}, func, ...args) => func.bind(null,
     Object.keys(dependencies).reduce((
-        reduction,
-        dependencyName
+        reduction, dependencyName
     ) => Object.assign(
         reduction, {
             [dependencyName]: require(dependencyName)
         }
-    )),
-    ...args
-)
+    )), ...args)
+
+// NOTE:
+    // Require your package.json file "require('./package.json')" as the first
+    // argument and your modules root function as the second argument followed
+    // by any additional arguments.
